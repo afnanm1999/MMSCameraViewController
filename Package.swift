9dbae658f6d4899755eb2187a89bf1a301a6d900
Afnan Mirza <afnanmirza1999@gmail.com>
2020-10-07 01:48:18 -0700

    1. Added SPM Support.

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MMSCameraViewController",
+    products: [
+        .library(
+            name: "MMSCameraViewController",
+            targets: ["MMSCameraViewController"]),
+    ],
+    targets: [
+        .target(
+            name: "MMSCameraViewController",
+            dependencies: []),
+    ]
+)
